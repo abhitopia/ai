fb77a749f287d944c9ba3cbc8a0be8e3b6aad94d
omkar varma <omkar@idelivr.com>
2016-02-13 21:04:44 +0530

    initial commit

---
+++ core/static/core/custom/home.js
@@ -0,0 +1,13 @@
+/**
+ * Created by omkar on 13/2/16.
+ */
+
+jQuery(document).ready(function() {
+
+    console.log("---document readey----");
+    jQuery(".navbar li a").on('click', function() {
+        console.log('---clicked-----');
+       jQuery('li').removeClass('active');
+       jQuery(this).parent('li').toggleClass('active');
+    });
+})
